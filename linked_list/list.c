b3b03caea22331cadd7ce0e90905aa6e696450f7
Christopher Berry <cnberry@gmail.com>
2016-09-20 20:11:09 -0400

    add files for linked list

---
+++ linked_list/list.c
@@ -0,0 +1,59 @@
+#include "list.h"
+
+int  ListLength(List* list) {
+	return 0;
+}
+
+int  ListEmpty(List* list) {
+	return 0;
+}
+
+int  ListAppend(List* list, void* ptr) {
+	return 0;
+}
+
+int  ListPrepend(List* list, void* ptr) {
+	return 0;	
+}
+
+void ListUnlink(List* list, ListElem* elem) {
+
+}
+
+void ListUnlinkAll(List* list) {
+
+}
+
+int  ListInsertAfter(List* list, void* ptr, ListElem* elem) {
+	return 0;
+}
+
+int  ListInsertBefore(List* list, void* ptr, ListElem* elem) {
+	return 0;
+}
+
+ListElem *ListFirst(List* list) {
+	return NULL;
+}
+
+ListElem *ListLast(List* list) {
+	return NULL;
+}
+
+ListElem *ListNext(List* list, ListElem* elem) {
+	return NULL;
+}
+
+ListElem *ListPrev(List* list, ListElem* elem) {
+	return NULL;
+}
+
+
+ListElem *ListFind(List* list, void* ptr) {
+	return NULL;
+}
+
+int ListInit(List* list) {
+	return 0;
+}
+
